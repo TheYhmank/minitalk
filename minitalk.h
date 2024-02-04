6ddcff3c8009765cf33e847e47de66f11019f429
Alibi <ayermeko@student.42prague.com>
2024-02-04 19:03:45 +0100

    modified files, list of tasks

--- minitalk.h
+++ minitalk.h
@@ -1,12 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ayermeko <ayermeko@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/04 16:42:45 by ayermeko          #+#    #+#             */
-/*   Updated: 2024/02/04 16:42:46 by ayermeko         ###   ########.fr       */
+/*   Created: 2024/02/04 16:47:25 by ayermeko          #+#    #+#             */
+/*   Updated: 2024/02/04 19:00:47 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <sys/types.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "libft/include/libft.h"
+
+#endif
